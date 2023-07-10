26a5110093f5ebf12d0e7fb8a19b4d493c8b8c82
Brandon Nestor Reis <brandon.nestor.reis@gmail.com>
2023-07-10 15:45:56 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.getElementById("main")
+console.log(main)
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Brandon is the champion"
